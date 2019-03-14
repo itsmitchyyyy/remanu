3c69443f09286e62b35d583cb1ed857a94ecfbcd
mitch alforque <malforque717@gmail.com>
2019-03-15 04:04:16 +0800

    updates

--- wp-content/themes/etereman/front-page.php
+++ wp-content/themes/etereman/front-page.php
@@ -1,8 +1,119 @@
 <?php get_header(); ?>
 <?php include('misc/carousel.php') ?>
+<div class="container">
+	<div class="row red-bg info-top">
+		<div class="col s4 info-image">
+			<img src="<?php echo get_template_directory_uri() ?>/img/etereman1985.png" alt="">
+		</div>
+		<div class="col s8 info-text">
+			<div class="info-text-top">
+				<span>
+				ETE REMAN is a Remanufacturer of Transmissions <br> 
+				and Transfer Cases For Domestic, Foreign, <br>
+				High Performance, and Exotic Cars and Trucks</span>
+			</div>
+			<div class="divider info-text-divider margin-t-1 margin-b-1"></div>
+			<div class="info-text-bottom">
+				<span>
+					ETE REMAN remanufactures over 4,000 different transmissions for over <br>
+					16,000 vehicle applications and has several regional warehouses throught the USA. <br>
+					ETE Reman also sells its own formula of fully synthetic transmission fluid.
+				</span>
+			</div>
+		</div>
+	</div>
+	<div class="row company-info">
+		<div class="col s12 center-align company-info-header">
+			<div class="company-info-header-text">
+				<h3>COMPANY OVERVIEW</h3>
+			</div>
+			<div class="company-info-header-video">
+				<iframe src="https://www.youtube.com/embed/w4mfZZvx7Os" frameborder="0"></iframe>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row services-list">
+	<div class="card">
+		<div class="col s6 services-info">
+			<div class="services-info-details" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/card-1.jpg)">
+				<div class="service-info-details-text">
+					<span>vehicle owners</span>
+				</div>
+				<div class="service-info-details-buttons">
+					<button class="btn waves-effect waves-light">milwaukee area auto centers</button>
+					<button class="btn waves-effect waves-light">auto centers throughout the u.s.</button>
+				</div>
+			</div>
+		</div>
+		<div class="col s6 services-info">
+			<div class="services-info-details" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/card-2.jpg)">
+				<div class="service-info-details-text">
+					<span>PRODUCT SUPPORT</span>
+				</div>
+				<div class="service-info-details-buttons">
+					<button class="btn waves-effect waves-light">learn more</button>
+				</div>
+			</div>
+		</div>
+		<div class="col s6 services-info">
+			<div class="services-info-details" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/card-3.jpg)">
+				<div class="service-info-details-text">
+					<span>buyete</span>
+				</div>
+				<div class="service-info-details-buttons">
+					<button class="btn waves-effect waves-light">shop now</button>
+				</div>
+			</div>
+		</div>
+		<div class="col s6 services-info">
+			<div class="services-info-details" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/card-4.jpg)">
+				<div class="service-info-details-text">
+					<span>careers</span>
+				</div>
+				<div class="service-info-details-buttons">
+					<button class="btn waves-effect waves-light">learn more</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row feedback-info">
+	<div class="card card-container" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/card-6.png)">
+		<img class="feedback-logo" src="<?php echo get_template_directory_uri() ?>/img/card-5.png" alt="">
+		<div class="feedback-header">
+			<div class="feedback-heading">
+				<span>what our customers saying</span>
+			</div>
+			<div class="container">
+				<div class="flexbox-row-center">
+					<div class="card feedback-card">
+						<div>
+							<span>Gordon Liske</span>
+							<span>Vehicle Owner</span>
+							<span>
+								Your team's effort in dealing with product issues, delivery, and customer service response times is truly outstanding.
+								Product failures happen but it is how you deal with them that counts
+							</span>
+						</div>
+					</div>
+					<div class="card feedback-card">
+						<div class="feedback-card-content">
+							<span>Gordon Liske</span>
+							<span>Vehicle Owner</span>
+							<span>
+								Your team's effort in dealing with product issues, delivery, and customer service response times is truly outstanding.
+								Product failures happen but it is how you deal with them that counts
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <h1><?php the_title(); ?></h1>
-
-<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-		<?php the_content(); ?>
+	<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+	<?php the_content(); ?>
 <?php endwhile; endif; ?>	
 <?php get_footer(); ?>
--- wp-content/themes/etereman/misc/carousel.php
+++ wp-content/themes/etereman/misc/carousel.php
@@ -1,27 +1,27 @@
 <div class="carousel carousel-slider center">
-    <div class="carousel-item white-text" style="background-image: url(https://lorempixel.com/250/250/nature/1)" gref="#one!">
+    <div class="carousel-item white-text" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/etereamanimage.jpg)" href="#one!">
         <div class="carousel-caption">
-            <h2>First Caption</h2>
+            <h2>The best remanufactured transmissions. Period.</h2>
         </div>
     </div>
-    <div class="carousel-item white-text" style="background-image: url(https://lorempixel.com/250/250/nature/2)" gref="#one!">
+    <div class="carousel-item white-text" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/etereamanimage.jpg)" href="#one!">
         <div class="carousel-caption">
-            <h2>Second Caption</h2>
+            <h2>The best remanufactured transmissions. Period.</h2>
         </div>
     </div>
-    <div class="carousel-item white-text" style="background-image: url(https://lorempixel.com/250/250/nature/3)" gref="#one!">
+    <div class="carousel-item white-text" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/etereamanimage.jpg)" href="#one!">
         <div class="carousel-caption">
-            <h2>Third Caption</h2>
+            <h2>The best remanufactured transmissions. Period.</h2>
         </div>
     </div>
-    <div class="carousel-item white-text" style="background-image: url(https://lorempixel.com/250/250/nature/4)" gref="#one!">
+    <div class="carousel-item white-text" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/etereamanimage.jpg)" href="#one!">
         <div class="carousel-caption">
-            <h2>Fourth Caption</h2>
+            <h2>The best remanufactured transmissions. Period.</h2>
         </div>
     </div>
-    <div class="carousel-item white-text" style="background-image: url(https://lorempixel.com/250/250/nature/5)" gref="#one!">
+    <div class="carousel-item white-text" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/etereamanimage.jpg)" href="#one!">
         <div class="carousel-caption">
-            <h2>Fifth Caption</h2>
+            <h2>The best remanufactured transmissions. Period.</h2>
         </div>
     </div>
 </div>
